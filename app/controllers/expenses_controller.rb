e0da4e41c8bd37dba7e3d7b3a4dcca50118b5364
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-11-19 21:22:23 +0000

    Se modifica filtro de busqueda

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,7 +2,7 @@ class ExpensesController < ApplicationController
   def index
   	p params
   	if params[:concept] or params[:category_id]
-      @expenses = Expense.search(params[:concept], params[:category_id]).order("date DESC")
+      @expenses = Expense.search(params[:concept], params[:category_id].to_i).order("date DESC")
     else
       @expenses = Expense.order("date DESC")
     end     
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,6 +3,10 @@ class Expense < ActiveRecord::Base
 
   # Metodo para la busqueda de productos por titulo o cuerpo de la pregunta
   def self.search(concept, category_id)
-    where("concept like ? AND category_id = ?", "%#{concept}%", "#{category_id}") 
+    if category_id != 0
+      where("concept like ? AND category_id = ?", "%#{concept}%", "#{category_id}") 
+    else
+      where("concept like ? ", "%#{concept}%") 
+    end
   end
 end
