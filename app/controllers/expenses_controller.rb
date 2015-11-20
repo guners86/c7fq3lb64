8c70f6e6b69f1929222bac29020024aaa7d8bfdc
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-11-20 15:01:05 +0000

    Se agrega filtro por usuario

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,10 +2,11 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    user = current_user
   	if params[:concept] or params[:category_id]
-  	  @expenses = Expense.search(params[:concept], params[:category_id].to_i).order("date DESC")
+  	  @expenses = Expense.search(user.id, params[:concept], params[:category_id].to_i).order("date DESC")
   	else
-  	  @expenses = Expense.order("date DESC")
+  	  @expenses = Expense.where("user_id = ? ", "#{user.id}").order("date DESC")
   	end   
   end
 end
